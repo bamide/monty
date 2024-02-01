8b0a91ddf7b1ae9ae55bdba66563b42b1c2ac5c7
bamide <bakremayowa2019@gmail.com>
2024-01-31 18:25:12 -0800

    update

---
+++ main.c
@@ -0,0 +1,52 @@
+#include "monty.h"
+
+bus_t bus = {NULL, NULL, NULL, 0};
+
+/**
+ * main - Monty code interpreter
+ * @argc: number of arguments
+ * @argv: Monty file location
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+    char *content = NULL;
+    FILE *file;
+    size_t size = 0;
+    stack_t *stack = NULL;
+    unsigned int counter = 0;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+
+    file = fopen(argv[1], "r");
+    bus.file = file;
+
+    if (!file)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    while (fgets(content, size, file) != NULL)
+    {
+        bus.content = content;
+        counter++;
+        if (size > 0)
+        {
+            execute(content, &stack, counter, file);
+        }
+        free(content);
+        content = NULL;
+        size = 0;
+    }
+
+    free_stack(stack);
+    fclose(file);
+
+    return 0;
+}
+
